91e12b12614d7ad348ca7817f52002c096e72204
Stefan Wojcik <stefan.wojcik@colorado.edu>
2022-03-15 20:22:25 -0500

    adding main file

--- src/mm2022.jl
+++ src/mm2022.jl
@@ -1,1 +1,45 @@
+module mm2022
+
+##############################################################################
+##
+## Dependencies
+##
+##############################################################################
+
+import CSV, DataFrames, Statistics, MLJ
+
+##############################################################################
+##
+## Exported methods and types
+##
+##############################################################################
+
+export  make_seeds,
+        get_seed_submission_diffs#,
+        #eff_stat_seasonal_means,
+        #get_eff_tourney_diffs,
+        #get_eff_submission_diffs,
+        #Elo,
+        #elo_ranks,
+        #get_elo_tourney_diffs,
+        #get_elo_submission_diffs,
+        #make_momentum,
+        #make_momentum_sub,
+        #make_ranef_features,
+        #make_ranef_sub
+
+
+##############################################################################
+##
+## Load files
+##
+##############################################################################
+
+#include("efficiency.jl")
+include("seeds.jl")
+#include("elo.jl")
+#include("momentum.jl")
+#include("teameffects.jl")
+
+end # module
 
